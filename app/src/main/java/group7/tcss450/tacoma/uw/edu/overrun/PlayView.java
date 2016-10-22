2e009e1527389dda2d7efc5fbc6c9a2a94250666
unladenSwallow <pedrol@uw.edu>
2016-10-22 09:40:11 -0700

    Added PlayView as in-game view Activity

---
+++ app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/PlayView.java
@@ -0,0 +1,13 @@
+package group7.tcss450.tacoma.uw.edu.overrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_view);
+    }
+}
