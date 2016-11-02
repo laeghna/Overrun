dd2e1b7337a81da776c0ecb39f45d5e6688dca88
Lisa Taylor <laeghna@uw.edu>
2016-11-01 17:46:45 -0700

    Create ZombieCrawler Class

--- app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/Zombie.java
+++ app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/Zombie.java
@@ -1,8 +1,26 @@
 package group7.tcss450.tacoma.uw.edu.overrun;
 
+import android.graphics.Bitmap;
+
 /**
- * Created by Lisa on 11/1/2016.
+ * This interface specifies the behavior for a zombie enemy.
+ *
+ * @author Lisa Taylor
+ * @version 1 Nov 2016
  */
 
 public interface Zombie {
+
+    //Resize the image
+    public Bitmap getResizedBmp(float newWidth, float newHeight);
+
+    public void updateMovement();
+
+    Bitmap getBitmap();
+
+    int getXCoord();
+
+    int getYCoord();
+
+    int getSpeed();
 }
---
+++ app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/ZombieCrawler.java
@@ -0,0 +1,83 @@
+package group7.tcss450.tacoma.uw.edu.overrun;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
+
+/**
+ * This class specifies the behavior for a zombie crawler enemy.
+ * This is the slowest enemy.
+ *
+ * @author Lisa Taylor
+ * @version 1 Nov 2016
+ */
+
+public class ZombieCrawler implements Zombie {
+
+    //Zombie crawler's speed
+    private static final int SPEED = 1;
+
+    //Zombie crawler image
+    private Bitmap crawlerBitmap;
+
+    //Zombie crawler coordinates
+    private int xCoord;
+    private int yCoord;
+
+    //Screen coordinate sto ensure enemy stays within screen
+    private int xMin;
+    private int xMax;
+
+    private int yMin;
+    private int yMax;
+
+    public ZombieCrawler(Context context, int screenX, int screenY) {
+
+        crawlerBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.zombie);
+
+        xMin = 0;
+        xMax = screenX;
+        yMin = 0;
+        yMax = screenY;
+
+        Random genRandom = new Random();
+
+        xCoord = screenX;
+        yCoord = genRandom.nextInt(yMax - crawlerBitmap.getHeight());
+    }
+
+    @Override
+    public Bitmap getResizedBmp(float newWidth, float newHeight) {
+        return null;
+    }
+
+    @Override
+    public void updateMovement() {
+
+        xCoord -= SPEED;
+
+        //do something if enemy reaches left edge
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return crawlerBitmap;
+    }
+
+    @Override
+    public int getXCoord() {
+        return xCoord;
+    }
+
+    @Override
+    public int getYCoord() {
+        return yCoord;
+    }
+
+    @Override
+    public int getSpeed() {
+        return SPEED;
+    }
+}
