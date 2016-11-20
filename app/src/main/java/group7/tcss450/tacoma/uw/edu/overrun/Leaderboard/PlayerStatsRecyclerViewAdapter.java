6c5c8c1af489bdaa6ffade49874e9125a0a858ed
dionmerz <dionmerz@gmail.com>
2016-11-20 11:39:39 -0800

    Scoreboard pulling from DB.

--- app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/Leaderboard/PlayerStatsRecyclerViewAdapter.java
+++ app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/Leaderboard/PlayerStatsRecyclerViewAdapter.java
@@ -12,16 +12,16 @@ import group7.tcss450.tacoma.uw.edu.overrun.Leaderboard.PlayerStats.PlayerStatsC
 import group7.tcss450.tacoma.uw.edu.overrun.R;
 
 /**
- * {@link RecyclerView.Adapter} that can display a {@link PlayerStatsContent.PlayerStats} and makes a call to the
+ * {@link RecyclerView.Adapter} that can display a {@link PlayerStatsContent} and makes a call to the
  * specified {@link PlayerStatsFragment.OnListFragmentInteractionListener}.
  * TODO: Replace the implementation with code for your data type.
  */
 public class PlayerStatsRecyclerViewAdapter extends RecyclerView.Adapter<PlayerStatsRecyclerViewAdapter.ViewHolder> {
 
-    private final List<PlayerStatsContent.PlayerStats> mValues;
+    private final List<PlayerStatsContent> mValues;
     private final PlayerStatsFragment.OnListFragmentInteractionListener mListener;
 
-    public PlayerStatsRecyclerViewAdapter(List<PlayerStatsContent.PlayerStats> items, PlayerStatsFragment.OnListFragmentInteractionListener listener) {
+    public PlayerStatsRecyclerViewAdapter(List<PlayerStatsContent> items, PlayerStatsFragment.OnListFragmentInteractionListener listener) {
         mValues = items;
         mListener = listener;
     }
@@ -36,19 +36,19 @@ public class PlayerStatsRecyclerViewAdapter extends RecyclerView.Adapter<PlayerS
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        holder.mIdView.setText(mValues.get(position).getPlayerId());
+        holder.mContentView.setText(mValues.get(position).getPlayerScore());
 
-        holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
-                }
-            }
-        });
+//        holder.mView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (null != mListener) {
+//                    // Notify the active callbacks interface (the activity, if the
+//                    // fragment is attached to one) that an item has been selected.
+//                    mListener.onListFragmentInteraction(holder.mItem);
+//                }
+//            }
+//        });
     }
 
     @Override
@@ -61,7 +61,7 @@ public class PlayerStatsRecyclerViewAdapter extends RecyclerView.Adapter<PlayerS
         public final TextView mIdView;
         public final TextView mContentView;
 
-        public PlayerStatsContent.PlayerStats mItem;
+        public PlayerStatsContent mItem;
 
         public ViewHolder(View view) {
             super(view);
