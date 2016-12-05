48e25f83c0027a60b3fcaf3a980123ce5f0eee11
dionmerz <dionmerz@gmail.com>
2016-12-04 21:07:55 -0800

    Fixed volume bug.

--- app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/Leaderboard/LeaderboardActivity.java
+++ app/src/main/java/group7/tcss450/tacoma/uw/edu/overrun/Leaderboard/LeaderboardActivity.java
@@ -84,12 +84,11 @@ public class LeaderboardActivity extends AppCompatActivity implements PlayerStat
         super.onResume();
 
         int music_position = mSharedPref.getInt(getString(R.string.music_position), 0);
-        double current_volume = mSharedPref.getFloat(
+        float current_volume = mSharedPref.getFloat(
                 getString(R.string.saved_volume_setting), 1);
 
-        // Set volume to an integer to properly display on Slider.
-        int volume_int = (int) (current_volume * 100);
-        mMediaPlayer.setVolume(volume_int,volume_int);
+
+        mMediaPlayer.setVolume(current_volume,current_volume);
         mMediaPlayer.seekTo(music_position);
         mMediaPlayer.start();
 
